cfc82a017c9d181a64c1288c8bb1006d72539dbe
Wahyu Agung Prasetyo <wahyu.agung.ft23@mail.umy.ac.id>
2025-11-07 21:33:37 +0700

    memperbaiki crash pada code mainactivity

--- app/src/main/java/com/example/navigasilayout/MainActivity.kt
+++ app/src/main/java/com/example/navigasilayout/MainActivity.kt
@@ -7,41 +7,40 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.navigasilayout.ui.theme.NavigasiLayoutTheme
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             NavigasiLayoutTheme {
+                val navController = rememberNavController()
+
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    NavHost(
+                        navController = navController,
+                        startDestination = "welcome",
                         modifier = Modifier.padding(innerPadding)
-                    )
+                    ) {
+                        composable("welcome") {
+                            WelcomeScreen(
+                                onSubmitClicked = { navController.navigate("form") }
+                            )
+                        }
+                        composable("form") {
+                            FormScreen()
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NavigasiLayoutTheme {
-        Greeting("Android")
     }
 }
